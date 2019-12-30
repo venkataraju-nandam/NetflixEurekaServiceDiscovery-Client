ab149bbe6bb9534095d979a6c8e4b344303b0eba
VenkataRaju Nandam <venkatarajunandam.infy@gmail.com>
2019-12-30 22:01:56 +0530

    initial commit of POC Springboot Netflix Eureka Service Discovery CLIENT

---
+++ src/main/java/com/poc/eureka/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,34 @@
+package com.poc.eureka.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+	
+//    @RequestMapping("/greeting")
+//    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+//        return new Greeting(counter.incrementAndGet(),
+//                            String.format(template, name));
+//    }
+	
+	
+    @ResponseBody
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Service";
+    }
+
+}
